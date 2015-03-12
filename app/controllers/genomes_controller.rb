4d21b42563d1539be7556854445510213eb92647
David Yao <njlxyaoxinwei@gmail.com>
2015-03-11 22:25:34 -0400

    more robust error handling

--- app/controllers/genomes_controller.rb
+++ app/controllers/genomes_controller.rb
@@ -2,14 +2,18 @@ class GenomesController < ApplicationController
   def query
     chr = params["chrom"]
     locs = params["locs"]
-    candidates = Genome.where(chrom: chr)
-    result = Array.new
-    locs.each_slice(100_000) do |slice|
-      result += candidates.where(loc: slice)
-    end
-    result.sort_by!(&:loc).map!(&:value)
-    respond_to do |format|
-      format.json {render json: result}
+    if chr.nil? || locs.nil?
+      render json: {
+        error: "Bad Query: chrom or locs missing",
+      }, status: 400
+    else
+      candidates = Genome.where(chrom: chr)
+      result = Array.new
+      locs.each_slice(100_000) do |slice|
+        result += candidates.where(loc: slice)
+      end
+      result.sort_by!(&:loc).map!(&:value)
+      render json: result
     end
   end
 end
