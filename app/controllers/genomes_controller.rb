65bcb1224aaa4954cac61e491119a12982244e3f
David Yao <njlxyaoxinwei@gmail.com>
2015-04-05 01:28:01 -0400

    updated query method

--- app/controllers/genomes_controller.rb
+++ app/controllers/genomes_controller.rb
@@ -7,13 +7,14 @@ class GenomesController < ApplicationController
         error: "Bad Query: chrom or locs missing",
       }, status: 400
     else
+      locs.map!(&:to_i)
+      locs.uniq!
+      locs.sort!
       candidates = Genome.where(chrom: chr)
       result = Array.new
       locs.each_slice(100_000) do |slice|
-        result += candidates.where(loc: slice)
+        result += candidates.where(loc: slice).order(loc: :asc).pluck(:value)
       end
-      result.sort_by!(&:loc).uniq!(&:loc)
-      result.map!(&:value)
       render json: result
     end
   end
